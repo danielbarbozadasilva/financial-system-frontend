c56b4a4931448b3d3edb03552a981e3c90e0abe9
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-01 16:26:31 -0300

    refactor: layout insertion strategy

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import Routers from './routers.js';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import Layout from './components/layout'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Layout/>
     <BrowserRouter>
       <Routers />
     </BrowserRouter>
