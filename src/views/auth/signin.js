ec197915debac6804942776e058efb881427e05a
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-03 00:02:45 -0300

    feat: create login interface

--- src/views/auth/signin.js
+++ src/views/auth/signin.js
@@ -1,30 +1,51 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Container, Alert, Row, Col, Form, Button } from 'react-bootstrap'
+import styled from 'styled-components'
 
 const SignIn = (props) => {
-    return (
-        <>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+  const [show, setShow] = useState(true)
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </>
+  return (
+    <Container>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <SForm>
+            <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+              <div>
+                <strong>OPS !!! </strong> Aconteceu um erro.
+              </div>
+              <small>Verifique CPF e senha</small>
+            </Alert>
+            <h2 tag="h4" className="text-login">
+              Login
+            </h2>
+            <Form.Group className="mb-3">
+              <Form.Label>CPF</Form.Label>
+              <Form.Control type="cpf" placeholder="Digite o seu CPF" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Senha</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Check type="checkbox" label="Lembrar credenciais" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Entrar
+            </Button>
+          </SForm>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 export default SignIn
+
+const SForm = styled(Form)`
+  background-color: #fafafa;
+  box-shadow: 0px 2px 15px 6px rgba(0, 0, 0, 0.11);
+  padding: 50px;
+  position: center;
+  margin-top: 80px;
+`
