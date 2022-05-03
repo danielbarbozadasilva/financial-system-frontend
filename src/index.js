ba67160afc8a8141599208c9a5f5e39b4f5ff2e7
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-03 00:02:13 -0300

    refactor: add style css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Routers from './routers.js';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import './assets/css/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
