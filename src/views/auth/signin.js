83736156bbdc501fd28d877478423a368c350926
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-24 18:56:58 -0300

    feat: create view signin

--- src/views/auth/signin.js
+++ src/views/auth/signin.js
@@ -1,39 +1,97 @@
-import React, { useState } from 'react'
-import { Container, Alert, Row, Col, Form, Button } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Container, Alert, Row, Col, Form, Button, Spinner } from 'react-bootstrap'
 import styled from 'styled-components'
+import { signInAction } from '../../store/auth/auth.action'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const SignIn = (props) => {
-  const [show, setShow] = useState(true)
+  const dispatch = useDispatch()
+  const [hasError, setHasError] = useState(false)
+  const error = useSelector((state) => state.auth.error)
+  const loading = useSelector((state) => state.auth.loading)
+
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (props) => {
+    const { value, name } = props.target
+    setForm({
+      ...form,
+      [name]: value
+    })
+  }
+
+  const submitForm = (event) => {
+    event.preventDefault()
+    dispatch(signInAction(form))
+  }
+
+  const isNotValid = () => form.email.length === 0 || form.password.length === 0
+
+  useEffect(() => {
+    setHasError(error.length > 0)
+  }, [error])
 
   return (
     <Container>
       <Row>
         <Col md={{ span: 6, offset: 3 }}>
           <SForm>
-            <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-              <div>
-                <strong>OPS !!! </strong> Aconteceu um erro.
-              </div>
-              <small>Verifique CPF e senha</small>
-            </Alert>
-            <h2 tag="h4" className="text-login">
-              Login
-            </h2>
+            <STextForm>Login</STextForm>
             <Form.Group className="mb-3">
-              <Form.Label>CPF</Form.Label>
-              <Form.Control type="cpf" placeholder="Digite o seu CPF" />
+              <Form.Label>E-mail</Form.Label>
+              <Form.Control
+                disabled={loading}
+                type="email"
+                name="email"
+                id="email"
+                onChange={handleChange}
+                value={form.email || ''}
+                placeholder="Informe o seu E-mail"
+              />
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Senha</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                disabled={loading}
+                type="password"
+                name="password"
+                id="password"
+                onChange={handleChange}
+                value={form.password || ''}
+                placeholder="Informe a sua senha"
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Check type="checkbox" label="Lembrar credenciais" />
             </Form.Group>
-            <Button variant="primary" type="submit">
-              Entrar
+            <Button
+              data-testing-id="funnel-survey-select_category-next"
+              type="button"
+              disabled={isNotValid()}
+              size="sm"
+              onClick={submitForm}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" animation="border" role="status">
+                    {' '}
+                    <span className="visually-hidden">Carregando...</span>
+                  </Spinner>
+                </>
+              ) : (
+                'Entrar'
+              )}
+
+              <i className="icon-angle-right ml-2" />
             </Button>
+            <SColFooter>
+              Não tem Cadastro? <Link to="/signup">Cadastre-se</Link>
+            </SColFooter>
           </SForm>
         </Col>
       </Row>
@@ -47,5 +105,19 @@ const SForm = styled(Form)`
   box-shadow: 0px 2px 15px 6px rgba(0, 0, 0, 0.11);
   padding: 50px;
   position: center;
-  margin-top: 80px;
+  margin: 80px 0px;
+`
+
+const SColFooter = styled(Col)`
+  line-height: 1.5;
+  font-weight: 500;
+  color: #000;
+  padding: 20px 0;
+`
+
+const STextForm = styled.h2`
+  line-height: 1.5;
+  font-weight: 500;
+  color: #000;
+  padding: 20px 0;
 `
