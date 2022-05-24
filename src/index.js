bfef7c2900daf188f5d571a786b0c9aae285e42c
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-24 16:21:15 -0300

    chore: add redux and redux thunk

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import Routers from './routers.js';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store'
 import './assets/css/style.css'
+import ReduxToastr from './components/redux-toastr'
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <ReduxToastr />
     <BrowserRouter>
       <Routers />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );
 reportWebVitals();
