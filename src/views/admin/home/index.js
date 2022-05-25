cac35b8e1e2d04d4b7e3a3f307e592a55f20c6ef
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-25 18:09:36 -0300

    refactor: style homepage admin

--- src/views/admin/home/index.js
+++ src/views/admin/home/index.js
@@ -1,22 +1,9 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
 
 function Home () {
-
-  const actions = () => null
-
   return (
     <>
-      <Title title='' actions={actions} />
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Grid container>
-            <Grid item>
-
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+      <h1>Admin</h1>
     </>
   )
 }
