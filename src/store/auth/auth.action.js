3cea0cda0cf37ce9d6a85c479fb2ba728f4e41f8
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-27 23:35:45 -0300

    feat: ensure that the user registers

--- src/store/auth/auth.action.js
+++ src/store/auth/auth.action.js
@@ -1,14 +1,13 @@
 import { removeToken, saveAuth } from '../../config/auth'
-import { authService } from '../../services/auth.service'
+import { authService, registerService } from '../../services/auth.service'
 import http from '../../config/http'
 import { navigate } from '@reach/router'
 import { toastr } from 'react-redux-toastr'
 import TYPES from '../types'
 
-export const signInAction = (data) => {
+export const signInAction = async (data) => {
   return async (dispatch) => {
     dispatch({ type: TYPES.SIGN_LOADING, status: true })
-
     try {
       const result = await authService(data)
       if (result.data) {
@@ -22,6 +21,25 @@ export const signInAction = (data) => {
       toastr.error('Usuário ou senha incorretos!')
       dispatch({ type: TYPES.SIGN_ERROR, data: error })
     }
+  }
+}
+
+export const signUpAction = async (data) => {
+  try {
+    const result = await registerService(data)
+    if (result.data) {
+      saveAuth(result.data.data)
+      http.defaults.headers.token = result.data.data.token
+    }
+    dispatch({
+      type: TYPES.SIGN_UP,
+      data: result.data
+    })
+    toastr.success('Usuário', 'cadastrado com sucesso!')
+    navigate('/admin')
+  } catch (error) {
+    toastr.error('Erro', 'Preencha todos os campos!')
+    dispatch({ type: TYPES.SIGN_ERROR, data: error })
   }
 }
 
