d777208babeaa7a8d5b24276c63399b5233d94e7
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-01 20:45:57 -0300

    refactor: update style main

---
+++ src/components/layout/main/MainElement.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Main = styled.div`
+  font-family: 'Nunito', Helvetica, Arial, Lucida, sans-serif;
+  font-size: 0.95rem;
+  line-height: 1.5;
+  font-weight: 500;
+  width: 85%;
+  flex: 1;
+  margin: 60px auto;
+  position: relative;
+  text-align: justify;
+  color: #771700;
+`
