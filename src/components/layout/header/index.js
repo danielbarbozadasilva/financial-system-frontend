755cace1a2bf172f29b10aadac4b9f37f30efa4e
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-01 20:45:36 -0300

    refactor: update style header

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Container, Nav } from 'react-bootstrap'
-import { SNavbar, SNavLink, SNavbarLogo } from './HeaderElements'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { SNavbar, SNavLink, SNavbarLogo, SNavbarToggle } from './HeaderElements'
 import LogoHeader from '../../../assets/img/header-image.png'
 import { Link } from '@reach/router'
 
@@ -11,7 +11,7 @@ const NavLink = (props) => (
       return {
         style: {
           textDecoration: isCurrent ? 'underline' : 'none',
-          color: 'white'
+          color: '#F1DEB8'
         }
       }
     }}
@@ -21,22 +21,28 @@ const NavLink = (props) => (
 const Header = () => {
   return (
     <>
-      <SNavbar>
-        <Link to="/" id="logoMain">
-          <SNavbarLogo className="logo-img" src={LogoHeader} alt="logo" />
-        </Link>
+      <SNavbar bg="light" expand="lg">
         <Container>
-          <Nav className="me-auto">
-            <SNavLink>
-              <NavLink to="/">Home</NavLink>
-            </SNavLink>
-            <SNavLink>
-              <NavLink to="/">Ativos</NavLink>
-            </SNavLink>
-            <SNavLink>
-              <NavLink to="/">Preços</NavLink>
-            </SNavLink>
-          </Nav>
+          <Navbar.Brand>
+            <Link to="/" id="logoMain">
+              <SNavbarLogo className="logo-img" src={LogoHeader} alt="logo" />
+            </Link>
+          </Navbar.Brand>
+          <SNavbarToggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <SNavLink>
+                <NavLink to="/">Home</NavLink>
+              </SNavLink>
+              <SNavLink>
+                <NavLink to="/">Ativos</NavLink>
+              </SNavLink>
+              <SNavLink>
+                <NavLink to="/">Preços</NavLink>
+              </SNavLink>
+            </Nav>
+          </Navbar.Collapse>
+
           <SNavbar.Collapse className="justify-content-end">
             <Nav>
               <SNavLink>
