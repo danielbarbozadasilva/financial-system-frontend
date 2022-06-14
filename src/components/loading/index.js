6b3d2c9216f1e8f4791b11a6b049dec5e28bb571
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-13 22:07:32 -0300

    refactor: change component path

---
+++ src/components/loading/index.js
@@ -0,0 +1,11 @@
+import { SLoading, LoadingBox } from './LoadingElements'
+
+const Loading = () => {
+  return (
+    <LoadingBox>
+      <SLoading />
+    </LoadingBox>
+  )
+}
+
+export default Loading
