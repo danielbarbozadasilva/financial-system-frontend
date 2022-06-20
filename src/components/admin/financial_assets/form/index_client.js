e273b1a65690f2d33859eb5c10990a17dd1d8787
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-06-20 17:38:16 -0300

    fix: final price calculation

--- src/components/admin/financial_assets/form/index_client.js
+++ src/components/admin/financial_assets/form/index_client.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
@@ -8,12 +7,17 @@ import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
-import { Button, Grid, LinearProgress } from '@material-ui/core'
-import { Submit } from './FormElements'
-import { getMoney } from '../../../../util/validations/price-validation'
+import { LinearProgress, Button } from '@material-ui/core'
+import {
+  calcSubTotal,
+  calcTotal
+} from '../../../../util/validations/price-validation'
+import { SInput, SBox, STable } from './FormElements'
 
 const FormClient = ({ submit, ...props }) => {
   const [form, setForm] = useState({})
+
+  const current_price = props.data.current_price
   const percent = useSelector((state) => state.financial.upload?.percent || 0)
 
   const handleSubmit = () => {
@@ -23,47 +27,15 @@ const FormClient = ({ submit, ...props }) => {
     submit(newForm)
   }
 
-  const calcSubTotal = (value) => {
-    var resp = parseFloat(
-      getMoney(props.data.current_price).replace('R$', '').replace(',', '.') *
-        value
-    ).toLocaleString('pt-br', {
-      style: 'currency',
-      currency: 'BRL'
-    })
-    return resp
-  }
-
-  const calcTotal = (current_price) => {
-    const TAX_RATE = 5
-    var price = parseFloat(
-      getMoney(current_price ? current_price : props.data.current_price)
-        .replace('R$', '')
-        .replace(',', '.')
-    )
-
-    var resp = price + (price / 100) * TAX_RATE
-    var convert = resp.toLocaleString('pt-br', {
-      style: 'currency',
-      currency: 'BRL'
-    })
-
-    if (!current_price) {
-      setForm({
-        total_price: convert
-      })
-    }
-
-    return convert
-  }
-
   const handleChange = (props) => {
     const { value, name } = props?.target
     if (name === 'quantity') {
-      var subTotal = calcSubTotal(value)
+      var subTotal = calcSubTotal(value, current_price)
+      var total = calcTotal(subTotal)
       setForm({
-        current_price: subTotal,
-        total_price: calcTotal(subTotal),
+        current_price: current_price,
+        subtotal_price: subTotal,
+        total_price: total,
         [name]: value
       })
     }
@@ -85,88 +57,85 @@ const FormClient = ({ submit, ...props }) => {
             <TableRow>
               <TableCell>{props.data.bvmf}</TableCell>
               <TableCell align="right">
-                <input
+                <SInput
+                  style={{ border: '0.5px solid grey' }}
                   type="number"
                   name="quantity"
                   onChange={handleChange}
                   min="1"
-                  max="9"
+                  max="5"
                   defaultValue={'1'}
                 />
               </TableCell>
-              <TableCell align="right">{props.data.current_price}</TableCell>
+              <TableCell align="right">{current_price}</TableCell>
               <TableCell align="right">
-                <input
+                <SInput
                   size="small"
                   margin="normal"
-                  name="current_price"
+                  name="subtotal_price"
                   type="text"
-                  value={form.current_price || props.data.current_price}
-                  disabled
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell rowSpan={1} />
-              <TableCell>Subtotal</TableCell>
-              <TableCell align="right">
-                <input
-                  size="small"
-                  margin="normal"
-                  name="current_price"
-                  type="text"
-                  value={form.current_price || props.data.current_price}
-                  disabled
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell rowSpan={1} />
-              <TableCell>Taxas</TableCell>
-              <TableCell align="right">
-                <input
-                  size="small"
-                  margin="normal"
-                  name="current_price"
-                  type="text"
-                  value={'5%'}
-                  disabled
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell rowSpan={1} />
-              <TableCell>
-                <strong>Total</strong>
-              </TableCell>
-              <TableCell align="right">
-                <input
-                  size="small"
-                  margin="normal"
-                  name="total_price"
-                  type="text"
-                  value={form.total_price || calcTotal(this)}
+                  value={form.subtotal_price || current_price}
                   disabled
                 />
               </TableCell>
             </TableRow>
           </TableBody>
         </Table>
+        <STable>
+          <TableRow>
+            <TableCell>Subtotal</TableCell>
+            <TableCell align="right">
+              <SInput
+                size="small"
+                margin="normal"
+                name="subtotal_price"
+                type="text"
+                value={form.subtotal_price || current_price}
+                disabled
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Taxas</TableCell>
+            <TableCell>
+              <SInput
+                size="small"
+                margin="normal"
+                name="current_tax"
+                type="text"
+                value={'5%'}
+                disabled
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <strong>Total</strong>
+            </TableCell>
+            <TableCell>
+              <SInput
+                size="small"
+                margin="normal"
+                name="total_price"
+                type="text"
+                value={form.total_price || calcTotal(current_price)}
+                disabled
+              />
+            </TableCell>
+          </TableRow>
+          <SBox>
+            <Button
+              size="small"
+              type="submit"
+              variant="contained"
+              onClick={handleSubmit}
+            >
+              Adquirir
+            </Button>
+          </SBox>
+        </STable>
+        <LinearProgress variant="determinate" value={percent} />
       </TableContainer>
-      <Submit>
-        <Button
-          align="right"
-          size="small"
-          type="submit"
-          variant="contained"
-          onClick={handleSubmit}
-        >
-          Adquirir
-        </Button>
-        <Grid container direction="column">
-          <LinearProgress variant="determinate" value={percent} />
-        </Grid>
-      </Submit>
     </>
   )
 }
--- src/util/validations/price-validation.js
+++ src/util/validations/price-validation.js
@@ -1,9 +1,30 @@
-export function getMoney (str) {
+export function getMoney(str) {
   const result = str?.replace('R$', '')
   return formatReal(parseInt(result?.replace(/[\D]+/g, '')))
 }
 
-function formatReal (int) {
+export function calcSubTotal(value, current_price){
+  var resp = parseFloat(
+    getMoney(current_price).replace('R$', '').replace(',', '.') * value
+  ).toLocaleString('pt-br', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+  return resp
+}
+
+export function calcTotal(subtotal) {
+  const TAX_RATE = 5
+  var price = parseFloat(getMoney(subtotal).replace('R$', '').replace(',', '.'))
+  var resp = price + (price / 100) * TAX_RATE
+  var convert = resp.toLocaleString('pt-br', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+  return convert
+}
+
+function formatReal(int) {
   let tmp = int + ''
   let neg = false
   if (tmp.indexOf('-') == 0) {
@@ -14,14 +35,25 @@ function formatReal (int) {
   if (tmp.length == 1) tmp = '0' + tmp
 
   tmp = tmp.replace(/([0-9]{2})$/g, ',$1')
-  if (tmp.length > 6) { tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, '.$1,$2') }
+  if (tmp.length > 6) {
+    tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, '.$1,$2')
+  }
 
-  if (tmp.length > 9) { tmp = tmp.replace(/([0-9]{3}).([0-9]{3}),([0-9]{2}$)/g, '.$1.$2,$3') }
+  if (tmp.length > 9) {
+    tmp = tmp.replace(/([0-9]{3}).([0-9]{3}),([0-9]{2}$)/g, '.$1.$2,$3')
+  }
 
-  if (tmp.length > 12) { tmp = tmp.replace(/([0-9]{3}).([0-9]{3}).([0-9]{3}),([0-9]{2}$)/g, '.$1.$2.$3,$4') }
+  if (tmp.length > 12) {
+    tmp = tmp.replace(
+      /([0-9]{3}).([0-9]{3}).([0-9]{3}),([0-9]{2}$)/g,
+      '.$1.$2.$3,$4'
+    )
+  }
 
   if (tmp.indexOf('.') == 0) tmp = tmp.replace('.', '')
   if (tmp.indexOf(',') == 0) tmp = tmp.replace(',', '0,')
 
-  return (neg ? '-' + 'R$' + tmp.replace('NaN', '') : 'R$' + tmp.replace('NaN', ''))
+  return neg
+    ? '-' + 'R$' + tmp.replace('NaN', '')
+    : 'R$' + tmp.replace('NaN', '')
 }
