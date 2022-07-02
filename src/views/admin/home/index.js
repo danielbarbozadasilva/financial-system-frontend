2a3a4b5441b549230efc50d259e26338c471e83b
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-07-02 10:05:45 -0300

    chore: add packages

--- src/views/admin/home/index.js
+++ src/views/admin/home/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import 'chart.js/auto';
-import { Chart } from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2'
 import { useDispatch, useSelector } from 'react-redux'
 import { Grid } from '@material-ui/core'
 import Title from '../../../components/title/index'
 import { listAllAssetAction } from '../../../store/financial_assets/financial_assets.action'
 import { listAllClientAction } from '../../../store/client/client.action'
 
-function Home () {
+function Home() {
   const dispatch = useDispatch()
 
   const financial = useSelector((state) => state.financial.all)
@@ -26,14 +25,8 @@ function Home () {
       {
         label: '# of Votes',
         data: [financial.length, client.length],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)'
-        ],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
         borderWidth: 1
       }
     ]
@@ -41,12 +34,12 @@ function Home () {
 
   return (
     <>
-    <Title title="Gráfico" actions={actions} />
+      <Title title="Gráfico" actions={actions} />
       <Grid container spacing={2}>
         <Grid item xs={6}>
           <Grid container>
             <Grid item>
-            <Chart type='pie' data={chartData} />
+              <Pie data={chartData} />
             </Grid>
           </Grid>
         </Grid>
