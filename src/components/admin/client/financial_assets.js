4742f2d98acc74cb619916015b3ebd9d8a0dcf30
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-07-11 19:37:28 -0300

    fix: state list financial assets

--- src/components/admin/client/financial_assets.js
+++ src/components/admin/client/financial_assets.js
@@ -4,10 +4,12 @@ import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
-
+import { useSelector } from 'react-redux'
 import DataList from '../../datagrid/index'
 
-const ListFinancialAssets = ({ open, close, assets }) => {
+const ListFinancialAssets = ({ ...props }) => {
+  const transactions = useSelector((state) => state.transaction.selected)
+
   const columnTransactionDetails = [
     {
       field: 'name',
@@ -31,17 +33,21 @@ const ListFinancialAssets = ({ open, close, assets }) => {
 
   return (
     <Dialog
-      open={open}
-      onClose={close}
-      aria-labelledby='alert-dialog-title'
-      aria-describedby='alert-dialog-description'
+      open={props.open}
+      onClose={props.close}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
     >
-      <DialogTitle id='alert-dialog-title'>Ativos adquiridos</DialogTitle>
+      <DialogTitle id="alert-dialog-title">Ativos adquiridos</DialogTitle>
       <DialogContent style={{ width: '500px ' }}>
-        <DataList data={assets} columns={columnTransactionDetails} loading={false} />
+        <DataList
+          data={transactions}
+          columns={columnTransactionDetails}
+          loading={false}
+        />
       </DialogContent>
       <DialogActions>
-        <Button onClick={close} color='primary' autoFocus>
+        <Button onClick={props.close} color="primary" autoFocus>
           fechar
         </Button>
       </DialogActions>
