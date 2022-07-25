dd9786e1a6e24e00256686b31b88ee699258a54d
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-07-25 17:42:17 -0300

    refactor: add container assets

--- src/views/portal/home/index.js
+++ src/views/portal/home/index.js
@@ -2,16 +2,22 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Loading from '../../../components/loading'
 import { Col } from 'react-bootstrap'
-import CardFinancial from '../../../components/portal/cards/index'
+import CardFinancial from '../../../components/portal/cards/financial/index'
+import CardAbout from '../../../components/portal/cards/about/index'
+import { navigate } from '@reach/router'
 import {
-  SContainer,
+  ContainerAssets,
+  ContainerFinancial,
+  ContainerText,
+  ContainerResources,
+  TextInvestiment,
   StyleImg,
-  SText,
-  Sh2,
+  ContainerTitle,
+  TextTitle,
   STextInvest,
+  SButtonTitle,
   settings
-} from '../../../components/portal/cards/styled'
-
+} from '../../../components/portal/cards/financial/styled'
 import { listAllAssetAction } from '../../../store/financial_assets/financial_assets.action'
 import Image from '../../../assets/img/investimento.webp'
 import Slider from 'react-slick'
@@ -44,22 +50,57 @@ function Home() {
       <div>
         <StyleImg src={Image} />
       </div>
-      <SText>
-        <Sh2>Acesso instantâneo a investimentos...</Sh2>
-        <Sh2>Invista nos ativos mais populares do mundo</Sh2>
-      </SText>
+      <ContainerTitle>
+        <TextTitle>
+          <h1>
+            <strong>Acesso instantâneo a investimentos...</strong>
+            <br />
+            Invista nos ativos mais populares do mundo
+          </h1>
+        </TextTitle>
+        <SButtonTitle onClick={() => navigate(`/signup`)}>ABRA SUA CONTA</SButtonTitle>
+      </ContainerTitle>
+
       <STextInvest>
-        <h2>
-          Escolha um <strong>investimento</strong>
-        </h2>
+        <h5>+ de 100 ativos disponíveis</h5>
+        <h1>
+          <strong>Invista nos ativos mais populares do mundo</strong>
+        </h1>
+        <h4>
+          De indústrias estabelecidas a emergentes de todos os ramos, escolha seu investimento e comece a lucrar
+        </h4>
       </STextInvest>
-      <SContainer>
+
+      <ContainerAssets>
         {!loading && financial.length === 0 ? (
           <h6>Não há Financiamentos disponiveis</h6>
         ) : (
           <Slider {...settings}>{FinancialList(financial)}</Slider>
         )}
-      </SContainer>
+      </ContainerAssets>
+
+      <ContainerFinancial>
+        <ContainerText>
+          <TextInvestiment>
+            <h1>
+              Os melhores <strong>ativos</strong>
+              <br />
+              na palma da sua mão
+            </h1>
+          </TextInvestiment>
+        </ContainerText>
+      </ContainerFinancial>
+
+      <ContainerResources>
+        <STextInvest>
+          <h1>
+            <strong>Recursos poderosos e fáceis de usar</strong>
+          </h1>
+          <h4>Veja o que alguns dos nossos usuários estão dizendo.</h4>
+          <CardAbout />
+          <SButtonTitle onClick={() => navigate(`/signup`)}>ABRA SUA CONTA</SButtonTitle>
+        </STextInvest>
+      </ContainerResources>
     </>
   )
 }
