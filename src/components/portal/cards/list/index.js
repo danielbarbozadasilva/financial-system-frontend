76fedd72c60511fc2bcd35d9755e5c28ef50be0f
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-08-19 12:20:10 -0300

    refactor: remove navigate

--- src/components/portal/cards/list/index.js
+++ src/components/portal/cards/list/index.js
@@ -1,6 +1,5 @@
 import { Card } from 'react-bootstrap'
 import { SCard } from './styled'
-import { navigate } from '@reach/router'
 
 const CardFinancial = (props) => {
   const { name, description, bvmf, count } = props.item
@@ -9,10 +8,11 @@ const CardFinancial = (props) => {
       <SCard style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>{name}</Card.Title>
-          <Card.Text>Descrição: {description}</Card.Text>
-          <Card.Text className="m-3 text-muted">BVMF: {bvmf}</Card.Text>
-          <Card.Text className="m-3 text-muted">Adiquirido: {count}</Card.Text>
-          <Card.Link onClick={() => navigate(`/signin`)}>Acessar</Card.Link>
+          <Card.Text className="text-muted">Descrição: {description}</Card.Text>
+          <Card.Text className="text-muted">BVMF: {bvmf}</Card.Text>
+          <Card.Text className="text-muted">
+            Adiquirido por: {count} cliente(s)
+          </Card.Text>
         </Card.Body>
       </SCard>
     </div>
